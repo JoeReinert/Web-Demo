ee07a106d5d9f9e5b1aeb4497742db23589e8eeb
JoeReinert <35753878+JoeReinert@users.noreply.github.com>
2019-01-16 13:11:26 -0600

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -2,5 +2,5 @@ $( document ).ready(function() {
   var bottomPad = $("footer").outerHeight();
   $("#content").css("padding-bottom", bottomPad + "px")
   var w = $("html").width();
-  $("#width").html("The width of the page is <em>" + w + "</em> pixels.");
+  $("#width").html("The width of the page is <b>" + w + "</b> pixels.");
 });
